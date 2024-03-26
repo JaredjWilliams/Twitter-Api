8d000c9f6b298dc5d4f74fdde0f59b4fa507a02d
Jared Williams <jared.williams101697@gmail.com>
2024-03-26 12:47:06 -0400

    (31, 32, 33) Added NotFoundException, NotAuthorizedException, BadRequestException

---
+++ socialmedia/src/main/java/com/cooksys/socialmedia/exceptions/NotAuthorizedException.java
@@ -0,0 +1,13 @@
+package com.cooksys.socialmedia.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class NotAuthorizedException extends RuntimeException {
+
+    private String message;
+}
