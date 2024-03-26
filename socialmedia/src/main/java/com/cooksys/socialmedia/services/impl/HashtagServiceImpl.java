e2c86f8d09ab47c99bb459f351cf2852a92492be
Jared Williams <jared.williams101697@gmail.com>
2024-03-26 12:38:37 -0400

    (37, 38, 39, 40) Added hashtag services plus impl, user service plus impl, tweet service plus impl, validate service plus impl

---
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/HashtagServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cooksys.socialmedia.services.impl;
+
+import com.cooksys.socialmedia.repositories.HashtagRepository;
+import com.cooksys.socialmedia.services.HashtagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HashtagServiceImpl implements HashtagService {
+
+    private HashtagRepository hashtagRepository;
+}
---
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/ValidateServiceImpl.java
@@ -0,0 +1,15 @@
+package com.cooksys.socialmedia.services.impl;
+
+import com.cooksys.socialmedia.repositories.UserRepository;
+import com.cooksys.socialmedia.services.ValidateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ValidateServiceImpl implements ValidateService {
+
+    private final UserRepository userRepository;
+
+
+}
