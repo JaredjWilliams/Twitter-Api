318f2ca39263f69f9e6f6cbf45bec6bcc8e1626d
Jared Williams <jared.williams101697@gmail.com>
2024-03-27 09:48:52 -0400

    18 - Added methods to get all undeleted users

--- socialmedia/src/main/java/com/cooksys/socialmedia/services/UserService.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/UserService.java
@@ -1,4 +1,9 @@
 package com.cooksys.socialmedia.services;
 
+import com.cooksys.socialmedia.dtos.user.UserResponseDto;
+
+import java.util.List;
+
 public interface UserService {
+    List<UserResponseDto> getUsers();
 }
