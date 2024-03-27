7af6385812c401523c49dadb236d9773e89bbf18
stanisripped <s_yu26@u.pacific.edu>
2024-03-27 11:24:13 -0700

    GetMapping for getting a user and throwing an exception if user is not found

--- socialmedia/src/main/java/com/cooksys/socialmedia/services/UserService.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/UserService.java
@@ -2,6 +2,7 @@ package com.cooksys.socialmedia.services;
 
 import com.cooksys.socialmedia.dtos.user.UserRequestDto;
 import com.cooksys.socialmedia.dtos.user.UserResponseDto;
+import com.cooksys.socialmedia.entities.User;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface UserService {
     List<UserResponseDto> getUsers();
 
     UserResponseDto createUser(UserRequestDto userCreationRequestDto);
+
+    UserResponseDto getUser(String username);
 }
